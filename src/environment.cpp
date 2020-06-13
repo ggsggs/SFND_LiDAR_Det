d267dca27d630c67bb0a9da7fcde9d9298bb1c05
Guillermo Garcia-Serrano <garcia.serrano.guillermo7@gmail.com>
2020-06-13 12:22:30 +0200

    Created pointer to lidar object in the heap.

--- src/environment.cpp
+++ src/environment.cpp
@@ -43,7 +43,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer) {
   std::vector<Car> cars = initHighway(renderScene, viewer);
 
   // TODO:: Create lidar sensor
-  
+  auto lidar = new Lidar(cars, 0);
   // TODO:: Create point processor
 }
 
