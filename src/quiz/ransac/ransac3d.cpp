add6ca8be3a470f1ac6386f779c607a20707ce90
Guillermo Garcia-Serrano <garcia.serrano.guillermo7@gmail.com>
2020-06-21 11:54:17 +0200

    Fixed ransac3d hidden error
    D was wrongly calculated as -v1.dot(normalV), where as it should be
    -p1.dot(normalV).
    
    Also increased efficiency of implementation:
    * Using 30% of points to rank candidate planes
    * Now only populating a container once the best plane is known.
    * Used heuristics to break loop assessing plane's fit to points. If
    remaining points in the loop for the current candidate are not enough to
    be the new best plane, break loop.
    
    TODO: no need to shuffle the whole vector. Randomly sample 30%without
    replacement from it.

--- src/quiz/ransac/ransac3d.cpp
+++ src/quiz/ransac/ransac3d.cpp
@@ -5,6 +5,7 @@
 #include "../../render/render.h"
 #include <unordered_set>
 #include <vector>
+#include <random>
 // using templates for processPointClouds so also include .cpp to help linker
 #include "../../processPointClouds.cpp"
 #include <Eigen/Dense>
@@ -42,9 +43,9 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData() {
   return cloud;
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D() {
-  ProcessPointClouds<pcl::PointXYZ> pointProcessor;
-  return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
+pcl::PointCloud<pcl::PointXYZI>::Ptr CreateData3D() {
+  ProcessPointClouds<pcl::PointXYZI> pointProcessor;
+  return pointProcessor.loadPcd("../../../sensors/data/pcd/data_1/0000000000.pcd");
 }
 
 pcl::visualization::PCLVisualizer::Ptr initScene() {
@@ -57,55 +58,79 @@ pcl::visualization::PCLVisualizer::Ptr initScene() {
   return viewer;
 }
 
-std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
                                int maxIterations, float distanceTol) {
+  using PointT = pcl::PointXYZI;
+  auto startTime = std::chrono::steady_clock::now();
   std::unordered_set<int> inliersResult;
-  srand(time(NULL));
 
-  // TODO: Fill in this function
+  // random config
+  const size_t min = 1;
+  const size_t max = cloud->points.size();
+  std::random_device rd;  
+  std::mt19937 gen(rd()); 
 
+  // vector of indices {0, 1, ..., size -1}
+  std::vector<int> indices(cloud->points.size() - 1);
+  std::iota(indices.begin(), indices.end(), 0);
+
+  // variables to store current best results
+  Eigen::Vector3d best_normalV;
+  float best_D;
+  int best_count = 0;
   // For max iterations
   for (int i = 0; i < maxIterations; i++) {
-    std::unordered_set<int> inliers;
-    // Randomly sample subset and fit line
-    int idx1 = rand() % cloud->points.size();
-    int idx2 = rand() % cloud->points.size();
-    int idx3 = rand() % cloud->points.size();
-    while (idx2 == idx1) {
-      idx2 = rand() % cloud->points.size();
-    }
-    while (idx3 == idx1 || idx3 == idx2) {
-      idx3 = rand() % cloud->points.size();
-    }
-    pcl::PointXYZ p1 = cloud->points[idx1];
-    pcl::PointXYZ p2 = cloud->points[idx2];
-    pcl::PointXYZ p3 = cloud->points[idx3];
+    std::shuffle(indices.begin(), indices.end(), gen);
 
-    Eigen::Vector3d v1{p2.x - p1.x, p2.y - p1.y, p2.z - p1.z};
-    Eigen::Vector3d v2{p3.x - p1.x, p3.y - p1.y, p3.z - p1.z};
-
+    // use first 3 points in shuffled vector to create the plane.
+    std::vector<Eigen::Vector3d> planePoints;
+    for (size_t i = 0; i < 3; i++){
+      auto p = cloud->points[indices[i]];
+      planePoints.push_back({p.x, p.y, p.z});
+    } 
+    // direction vectors that define plane, normal vector to it, distnce to origin.
+    Eigen::Vector3d v1 = planePoints.at(1) - planePoints.at(0);//{p2.x - p1.x, p2.y - p1.y, p2.z - p1.z};
+    Eigen::Vector3d v2 = planePoints.at(2) - planePoints.at(0);//{p3.x - p1.x, p3.y - p1.y, p3.z - p1.z};
     auto normalV = v1.cross(v2);
-    float D = -(v1.dot(normalV));
-
+    float D = -(planePoints.at(0).dot(normalV));
     float denominator = normalV.norm();
 
-    for (int idx = 0; idx < cloud->points.size(); idx++) {
+    uint count = 0;   // count of number of points that fit the plane.
+    uint maxIters = indices.size()*30/100+1;
+    for (int idx : indices) {
       auto p = cloud->points[idx];
       Eigen::Vector3d v{p.x, p.y, p.z};
       // Measure distance between every point and fitted line
       float d = fabs(v.dot(normalV) + D) / denominator;
       // If distance is smaller than threshold count it as inlier
-      // std::cout << d << std::endl;
-      if (d <= distanceTol) {
-        inliers.insert(idx);
-      }
+      maxIters--;
+      if (d <= distanceTol) count++;
+      if (maxIters == 0 || maxIters + count < best_count) break;
     }
 
-    if (inliers.size() > inliersResult.size()) {
-      inliersResult = inliers;
+    if (count > best_count) { // update best values if candidate is superior.
+      best_normalV = normalV;
+      best_D = D;
+      best_count = count;
     }
   }
-  // Return indicies of inliers from fitted line with most inliers
+
+  // generate output container once best plane candidate is known.
+  for (int idx = 0; idx < cloud->points.size(); idx++) {
+    auto p = cloud->points[idx];
+    Eigen::Vector3d v{p.x, p.y, p.z};
+    // Measure distance between every point and fitted line
+    float d = fabs(v.dot(best_normalV) + best_D) / best_normalV.norm();
+    // If distance is smaller than threshold count it as inlier
+    if (d <= distanceTol) {
+      inliersResult.insert(idx);
+    }
+  }
+  auto endTime = std::chrono::steady_clock::now();
+  auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+      endTime - startTime);
+  std::cout << "plane segmentation took " << elapsedTime.count()
+            << " milliseconds" << std::endl;
   return inliersResult;
 }
 
@@ -115,20 +140,29 @@ int main(int argc, const char *argv[]) {
   pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
   // Create data
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = CreateData3D();
+
+  const Eigen::Vector4f minPoint{-40, -7, -3, 1};
+  const Eigen::Vector4f maxPoint{40, 7, 1, 1};
+  const float resolution = 0.2;
+
+  ProcessPointClouds<pcl::PointXYZI>* pointProcessor =
+      new ProcessPointClouds<pcl::PointXYZI>();
+  auto filterCloud =
+      pointProcessor->FilterCloud(cloud, resolution, minPoint, maxPoint);
 
   // TODO: Change the max iteration and distance tolerance arguments for Ransac
   // function
   std::unordered_set<int> inliers =
-      Ransac(cloud, std::atoi(argv[1]), std::atof(argv[2]));
+      Ransac(filterCloud, std::atoi(argv[1]), std::atof(argv[2]));
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudInliers(
-      new pcl::PointCloud<pcl::PointXYZ>());
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(
-      new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloudInliers(
+      new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloudOutliers(
+      new pcl::PointCloud<pcl::PointXYZI>());
 
-  for (int index = 0; index < cloud->points.size(); index++) {
-    pcl::PointXYZ point = cloud->points[index];
+  for (int index = 0; index < filterCloud->points.size(); index++) {
+    pcl::PointXYZI point = filterCloud->points[index];
     if (inliers.count(index))
       cloudInliers->points.push_back(point);
     else
